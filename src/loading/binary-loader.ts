585fed9bf821ea63cfe157cc6d4ee7f3a44bbc5f
Atanas Georgiev <atanas.georgiev@navvis.com>
2022-01-25 17:58:03 +0100

    Fixed an issue where Chromium based browsers will crash because of too many web workers when loading a large number of pointclouds at once. The problem is fixed by utilizing a pool of reusable and self-terminating workers.

--- src/loading/binary-loader.ts
+++ src/loading/binary-loader.ts
@@ -5,6 +5,7 @@
 import { Box3, BufferAttribute, BufferGeometry, Uint8BufferAttribute, Vector3 } from 'three';
 import { PointAttributeName, PointAttributeType } from '../point-attributes';
 import { PointCloudOctreeGeometryNode } from '../point-cloud-octree-geometry-node';
+import { WorkerPool } from '../utils/worker-pool';
 import { Version } from '../version';
 import { GetUrlFn, XhrRequest } from './types';
 
@@ -46,7 +47,10 @@ export class BinaryLoader {
   xhrRequest: XhrRequest;
   callbacks: Callback[];
 
-  private workers: Worker[] = [];
+  private static readonly WORKER_POOL = new WorkerPool(
+    32,
+    require('../workers/binary-decoder.worker.js'),
+  );
 
   constructor({
     getUrl = s => Promise.resolve(s),
@@ -69,9 +73,6 @@ export class BinaryLoader {
   }
 
   dispose(): void {
-    this.workers.forEach(worker => worker.terminate());
-    this.workers = [];
-
     this.disposed = true;
   }
 
@@ -107,71 +108,56 @@ export class BinaryLoader {
       return;
     }
 
-    const worker = this.getWorker();
+    BinaryLoader.WORKER_POOL.getWorker().then(autoTerminatingWorker => {
+      const pointAttributes = node.pcoGeometry.pointAttributes;
+      const numPoints = buffer.byteLength / pointAttributes.byteSize;
 
-    const pointAttributes = node.pcoGeometry.pointAttributes;
-    const numPoints = buffer.byteLength / pointAttributes.byteSize;
-
-    if (this.version.upTo('1.5')) {
-      node.numPoints = numPoints;
-    }
-
-    worker.onmessage = (e: WorkerResponse) => {
-      if (this.disposed) {
-        resolve();
-        return;
+      if (this.version.upTo('1.5')) {
+        node.numPoints = numPoints;
       }
 
-      const data = e.data;
+      autoTerminatingWorker.worker.onmessage = (e: WorkerResponse) => {
+        if (this.disposed) {
+          resolve();
+          BinaryLoader.WORKER_POOL.releaseWorker(autoTerminatingWorker);
+          return;
+        }
 
-      const geometry = (node.geometry = node.geometry || new BufferGeometry());
-      geometry.boundingBox = node.boundingBox;
+        const data = e.data;
 
-      this.addBufferAttributes(geometry, data.attributeBuffers);
-      this.addIndices(geometry, data.indices);
-      this.addNormalAttribute(geometry, numPoints);
+        const geometry = (node.geometry = node.geometry || new BufferGeometry());
+        geometry.boundingBox = node.boundingBox;
 
-      node.mean = new Vector3().fromArray(data.mean);
-      node.tightBoundingBox = this.getTightBoundingBox(data.tightBoundingBox);
-      node.loaded = true;
-      node.loading = false;
-      node.failed = false;
-      node.pcoGeometry.numNodesLoading--;
-      node.pcoGeometry.needsUpdate = true;
+        this.addBufferAttributes(geometry, data.attributeBuffers);
+        this.addIndices(geometry, data.indices);
+        this.addNormalAttribute(geometry, numPoints);
 
-      this.releaseWorker(worker);
+        node.mean = new Vector3().fromArray(data.mean);
+        node.tightBoundingBox = this.getTightBoundingBox(data.tightBoundingBox);
+        node.loaded = true;
+        node.loading = false;
+        node.failed = false;
+        node.pcoGeometry.numNodesLoading--;
+        node.pcoGeometry.needsUpdate = true;
 
-      this.callbacks.forEach(callback => callback(node));
-      resolve();
-    };
+        this.callbacks.forEach(callback => callback(node));
+        resolve();
+        BinaryLoader.WORKER_POOL.releaseWorker(autoTerminatingWorker);
+      };
 
-    const message = {
-      buffer,
-      pointAttributes,
-      version: this.version.version,
-      min: node.boundingBox.min.toArray(),
-      offset: node.pcoGeometry.offset.toArray(),
-      scale: this.scale,
-      spacing: node.spacing,
-      hasChildren: node.hasChildren,
-    };
+      const message = {
+        buffer,
+        pointAttributes,
+        version: this.version.version,
+        min: node.boundingBox.min.toArray(),
+        offset: node.pcoGeometry.offset.toArray(),
+        scale: this.scale,
+        spacing: node.spacing,
+        hasChildren: node.hasChildren,
+      };
 
-    worker.postMessage(message, [message.buffer]);
-  }
-
-  private getWorker(): Worker {
-    const worker = this.workers.pop();
-    if (worker) {
-      return worker;
-    }
-
-    const ctor = require('../workers/binary-decoder.worker.js');
-
-    return new ctor();
-  }
-
-  private releaseWorker(worker: Worker): void {
-    this.workers.push(worker);
+      autoTerminatingWorker.worker.postMessage(message, [message.buffer]);
+    });
   }
 
   private getTightBoundingBox({ min, max }: { min: number[]; max: number[] }): Box3 {
