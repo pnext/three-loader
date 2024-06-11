78d82fc4d14d392e40376ca99b167643e5b3784c
Martin Panknin <martinpanknin@gmail.com>
2024-06-11 10:47:54 +0200

    Update src/loading2/load-octree.ts
    Co-authored-by: Piotr Jurczyński <619650+pjurczynski@users.noreply.github.com>

--- src/loading2/load-octree.ts
+++ src/loading2/load-octree.ts
@@ -7,7 +7,6 @@ export async function loadOctree(
 	xhrRequest: XhrRequest,
 ) {
 	const trueUrl = await getUrl(url);
-	
 	const loader = new OctreeLoader(getUrl, url);
 	const {geometry} = await loader.load(trueUrl, xhrRequest);
 
