a1ad1291238d4a2efeac0d85ead36da3b6b3d967
Martin Panknin <martin.panknin@pix4d.com>
2024-06-24 12:09:32 +0200

    fix: bounds in decoder

--- src/loading2/decoder.worker.js
+++ src/loading2/decoder.worker.js
@@ -45,6 +45,11 @@ onmessage = function (event) {
     };
 
     let numOccupiedCells = 0;
+
+    // Initialize tight bounding box
+    let tightBoxMin = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
+    let tightBoxMax = [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY];
+
     for (let pointAttribute of pointAttributes.attributes) {
         
         if(['POSITION_CARTESIAN', 'position'].includes(pointAttribute.name)){
@@ -58,6 +63,15 @@ onmessage = function (event) {
                 let x = (view.getInt32(pointOffset + attributeOffset + 0, true) * scale[0]) + offset[0] - min.x;
                 let y = (view.getInt32(pointOffset + attributeOffset + 4, true) * scale[1]) + offset[1] - min.y;
                 let z = (view.getInt32(pointOffset + attributeOffset + 8, true) * scale[2]) + offset[2] - min.z;
+
+                // Update tight bounding box
+                tightBoxMin[0] = Math.min(tightBoxMin[0], x);
+                tightBoxMin[1] = Math.min(tightBoxMin[1], y);
+                tightBoxMin[2] = Math.min(tightBoxMin[2], z);
+
+                tightBoxMax[0] = Math.max(tightBoxMax[0], x);
+                tightBoxMax[1] = Math.max(tightBoxMax[1], y);
+                tightBoxMax[2] = Math.max(tightBoxMax[2], z);
 
                 let index = toIndex(x, y, z);
                 let count = grid[index]++;
@@ -196,6 +210,7 @@ onmessage = function (event) {
         buffer: buffer,
         attributeBuffers: attributeBuffers,
         density: occupancy,
+        tightBoundingBox: { min: tightBoxMin, max: tightBoxMax },
     };
 
     let transferables = [];
