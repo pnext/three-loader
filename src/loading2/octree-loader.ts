4cbb1e6ad056b5b5d65475f4c08ef2523c5274e7
Martin Panknin <martin.panknin@pix4d.com>
2024-07-03 13:29:15 +0200

    fix: bounds min as offset

--- src/loading2/octree-loader.ts
+++ src/loading2/octree-loader.ts
@@ -479,7 +479,7 @@ export class OctreeLoader {
 			);
 		}
 
-		const offset = metadata.offset;
+		const offset = metadata.boundingBox.min;
 		const tightBoundingBox = new Box3(
 			new Vector3(
 				positionAttribute.min[0] - offset[0],
