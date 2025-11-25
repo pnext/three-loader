414c02b158bbef60b0c2ce9d3c2c64c0621f6ca5
Piotr Jurczyński <619650+pjurczynski@users.noreply.github.com>
2025-11-25 19:35:21 +0100

    fix: remove major vulnerabilities from the project (#243)

--- src/works.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-
-describe('smoke test for a testing environment', () => {
-  test('jest is configured and runs without errors', () => {
-    expect(true).toBe(true);
-  });
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const SizePlugin = require('size-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -37,5 +36,5 @@ module.exports = {
       { test: /\.(vs|fs|glsl|vert|frag)$/, loader: 'raw-loader' },
     ],
   },
-  plugins: [new SizePlugin()],
+  plugins: [],
 };
